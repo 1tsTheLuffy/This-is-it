d841cdd22c816bbc6e93b563659f373e64d9b648
1tsTheLuffy <58876026+1tsTheLuffy@users.noreply.github.com>
2020-01-05 14:06:06 +0530

    Second Commit..

---
+++ Assets/Top Down Shooter Assets/Scripts/Spaceship.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spaceship : MonoBehaviour
+{
+    private Vector2 mousePos;
+    private Vector2 movement;
+    [SerializeField] float movementSpeed;
+
+    Rigidbody2D rb;
+    Camera cam;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        cam = Camera.main;
+    }
+
+    private void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = new Vector2(mousePos.x - transform.position.x, mousePos.y - transform.position.y);
+        transform.up = direction;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(movement.x * movementSpeed * Time.fixedDeltaTime, 
+            movement.y * movementSpeed * Time.fixedDeltaTime);
+    }
+}
